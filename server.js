80ae2f3bce8561cd86d4e5d7347e066555d69fff
Danny Rizko <drizko07@gmail.com>
2016-03-05 23:14:45 -0800

    Fixes production port in server.js

--- server.js
+++ server.js
@@ -5,11 +5,13 @@ var bodyParser = require('body-parser');
 
 var app = express();
 
+var port = process.env.PORT || 3000;
+
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.listen(3000, function () {
+app.listen(process.env.PORT, function () {
   console.log('Example app listening on port 3000!');
 });
