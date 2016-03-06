1c69659bbe9cf03cec0ac1942d6c8f06583b7c39
Danny Rizko <drizko07@gmail.com>
2016-03-05 23:12:49 -0800

    Removes unneeded package

--- server.js
+++ server.js
@@ -1,13 +1,10 @@
 var express = require('express');
 var path = require('path');
 var logger = require('morgan');
-var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var app = express();
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
